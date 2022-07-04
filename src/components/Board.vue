b237aa14db4cf779956b2b6a0637b19411e95ab5
James Sinkala <xinnks@gmail.com>
2022-07-04 06:38:51 +0300

    feat: Add board component

---
+++ src/components/Board.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { computed } from "vue";
+import { useStore } from "vuex";
+import Column from "./Column.vue"
+
+const store = useStore()
+
+defineProps({
+  msg: String
+})
+
+const todoList = computed(() => store.getters.allTasksByType("todo"))
+const inProgressList = computed(() => store.getters.allTasksByType("in-progress"))
+const doneList = computed(() => store.getters.allTasksByType("done"))
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+
+  <div class="board">
+    <Column type="todo" title="Todo" :tasks="todoList"/>
+    <Column type="in-progress" title="In Progress" :tasks="inProgressList"/>
+    <Column type="done" title="Done" :tasks="doneList"/>
+  </div>
+</template>
+
+<style scoped>
+a {
+  color: #42b983;
+}
+div.board{
+  position: relative;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  height: 50vh;
+}
+</style>
