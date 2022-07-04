6a782c2212dda78abee7732c6867a3743fd42e08
James Sinkala <xinnks@gmail.com>
2022-07-04 06:50:48 +0300

    feat: Append kanban board to base component

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,10 @@
+<script setup>
+import Board from './components/Board.vue'
+</script>
+
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
+  <Board msg="Vue Kanban" />
 </template>
 
 <style>
@@ -10,5 +15,9 @@
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+img{
+  width: 100px;
+  height: auto;
 }
 </style>
